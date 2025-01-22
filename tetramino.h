58e6df84bc1008399cf931fafbfe04cf5f38106c
Chipskein <abfn0905@gmail.com>
2025-01-21 21:32:49 -0300

    feat:Block movimentation + Desktop input

--- tetramino.h
+++ tetramino.h
@@ -3,9 +3,12 @@
 #include <time.h>
 #include <stdlib.h>
 enum Shape { T, O, J, L, I, S, Z};
+enum Direction {LEFT,RIGHT,DOWN};
 typedef struct{
     Block **blocks;
     enum Shape shape;
+    bool stopped;
+    int angle;//0 90 180 270
 } Tetramino;
 Tetramino *CreateTetramino(enum Shape shape, Color color,int x, int y);
 Tetramino *CreateRandomTetramino(int x, int y);
@@ -13,3 +16,5 @@ void DestroyTetramino(Tetramino *tetramino);
 void DrawTetramino(Tetramino *tetramino,Block *playFieldBlocks[ROWS][COLS]);
 int GetRandomShape(void);
 Color GetRandomColor(void);
+void Rotate(Tetramino *tetramino,Block *playFieldBlocks[ROWS][COLS]);
+void Move(Tetramino *tetramino,enum Direction direction,Block *playFieldBlocks[ROWS][COLS]);
